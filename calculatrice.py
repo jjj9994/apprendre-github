2a27f567db15e9e8db9ec7a6ebc2ffdf4d98e499
Jean Garcia <jeangarcia1394@gmail.com>
2026-07-08 16:28:41 +0200

    Ajoute une calculatrice simple

---
+++ calculatrice.py
@@ -0,0 +1,11 @@
+a = float(input("Premier nombre : "))
+b = float(input("Deuxième nombre : "))
+
+print("Somme :", a + b)
+print("Différence :", a - b)
+print("Produit :", a * b)
+
+if b != 0:
+    print("Division :", a / b)
+else:
+    print("Division impossible par zéro.")
