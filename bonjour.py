2b10d726d91ec08c2ca7192901faa74b2ac809b5
Jean Garcia <jeangarcia1394@gmail.com>
2026-07-08 16:02:26 +0200

    Ajoute une saisie du nom utilisateur

--- bonjour.py
+++ bonjour.py
@@ -1,3 +1,5 @@
-print("Bonjour, GitHub !")
-print("Je modifie ce fichier depuis mon ordinateur.")
-print("Ensuite je vais envoyer cette modification sur GitHub.")
+nom = input("Comment tu t'appelles ? ")
+
+print("Bonjour", nom)
+print("Je lance ce programme depuis mon ordinateur.")
+print("Cette modification a été faite sur une branche.")
