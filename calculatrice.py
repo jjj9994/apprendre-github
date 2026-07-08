c18105f8af6df86cb9510e9da1fea7bc6273005b
Jean Garcia <jeangarcia1394@gmail.com>
2026-07-08 16:58:21 +0200

    Ameliore la calculatrice avec une boucle

--- calculatrice.py
+++ calculatrice.py
@@ -1,11 +1,27 @@
-a = float(input("Premier nombre : "))
-b = float(input("Deuxième nombre : "))
+while True:
+    entree_a = input("Premier nombre ou q pour quitter : ")
 
-print("Somme :", a + b)
-print("Différence :", a - b)
-print("Produit :", a * b)
+    if entree_a == "q":
+        print("Fin du programme.")
+        break
 
-if b != 0:
-    print("Division :", a / b)
-else:
-    print("Division impossible par zéro.")
+    entree_b = input("Deuxième nombre : ")
+
+    try:
+        a = float(entree_a)
+        b = float(entree_b)
+
+        print("Somme :", a + b)
+        print("Différence :", a - b)
+        print("Produit :", a * b)
+
+        if b != 0:
+            print("Division :", a / b)
+        else:
+            print("Division impossible par zéro.")
+
+        print("---")
+
+    except ValueError:
+        print("Erreur : tu dois entrer des nombres.")
+        print("---")
